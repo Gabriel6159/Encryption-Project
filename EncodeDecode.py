492a44796c1fdc75946f512a93ab7d0ad7b09d2a
Gabriel6159 <122646844+Gabriel6159@users.noreply.github.com>
2023-04-21 14:34:50 -0500

    Update EncodeDecode.py

--- EncodeDecode.py
+++ EncodeDecode.py
@@ -1,14 +1,13 @@
 '''
-For some reason when I try to multiply two numbers on line 34, the program spits out an error
-It says that charNum is a NoneType variable, and I have no idea why
+Changes have been made, it seems to almost work, but it outputs none instead of a bracket when I decrypt it
 '''
 #Extras if time:
 #	Spaces can be a range of numbers multiplied by key or a symbol not used
 #	encryption thing in math module
 #	add fancy user interface stuff
 #Key is a class
-charList=['A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h','I','i','J','j','K','k','L','l','M','m','N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z',',','.','?',' ','-',':',';','(',')','[',']',"'",'$','!','@','#','%','^','&','*','=','+','_','/','<','>','1','2','3','4','5','6','7','8','9','0']
-translate=[96621, 85176, 51578, 40025, 85009, 68488, 47828, 59844, 77011, 14508, 87030, 52496, 24778, 75276, 10496, 72109, 92165, 55647, 71374, 26857, 69507, 46554, 40196, 61950, 64970, 16497, 31844, 50081, 46858, 89744, 55558, 16503, 87246, 37369, 92408, 12133, 30486, 40010, 85201, 28803, 66263, 38617, 69730, 11987, 54091, 56776, 14046, 46609, 81196, 21992, 46863, 78943, 12722, 54081, 46113, 90634, 19807, 79900, 51849, 89614, 58739, 80577, 78654, 72818, 15499, 23438, 13959, 83925, 73204, 51189, 49322, 87313, 72840, 54554, 10547, 78939, 39540, 95015, 79447, 45918, 71512, 87657, 24733, 46241, 92435, 63909, 49341, 57261]
+charList=['A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h','I','i','J','j','K','k','L','l','M','m','N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z',',','.','?',' ','-',':',';','(',')','[',']',"'",'$','!','@','#','%','^','&','*','=','+','_','	','/','<','>','1','2','3','4','5','6','7','8','9','0']
+translate=[62870, 28843, 74415, 79187, 76794, 54492, 87980, 42159, 48728, 67409, 64116, 99315, 10236, 40528, 14389, 80359, 61460, 15988, 14228, 74688, 95523, 60498, 90755, 55723, 33767, 18099, 85402, 24640, 56315, 67634, 58253, 32566, 41366, 10016, 91929, 78292, 54453, 69143, 53827, 81241, 81176, 72843, 88030, 54877, 21789, 77985, 37618, 60138, 85550, 39229, 58116, 80927, 29307, 78299, 86306, 13533, 98228, 98175, 84568, 71261, 60879, 99984, 66576, 27513, 23978, 79640, 13388, 37810, 15164, 69054, 12469, 32458, 13139, 66883, 61423, 68991, 36234, 40576, 74661, 25283, 30795, 75537, 18427, 24259, 98192, 71752, 92272, 11527, 65589]
 class Key:
 	def __init__(self,key,length,firstChar):
 		self.key=key
@@ -31,16 +30,17 @@ def charToInt(char):
 			return translate[n]
 def intToChar(number):
 	for n in range(0,len(translate)):
-		if number==translate(n):
-			return charList(n)
+		if number==translate[n]:
+			return charList[n]
 #Encodes numbers derived from characters
 def charNumEncode(charNum):
+	print(charNum)
 	step1=charNum*keyObj.keyNum() #For some reason charNum is of type NoneType
 	step2=keyObj.firstCharNum()**2
 	step3=(step1+step2)/keyObj.firstCharNum()
 	return step3
 def charNumDecode(charNum):
-	step1=charNum*key.Obj.firstCharNum()
+	step1=float(charNum)*keyObj.firstCharNum()#Entire decrypted file was none
 	step2=step1-(keyObj.firstCharNum()**2)
 	step3=step2/keyObj.keyNum()
 	return step3
@@ -56,7 +56,7 @@ encodeDecode=input('Encrypt or Decrypt? (E or D): ')
 #key=input('Enter Key: ')
 key='32df9v0283uj' #Hardcoded Key
 keyObj=Key(key,len(key),key[0])
-filename='EncodeThis'#input('Enter File Name (do not add extension): ')
+filename='EncryptedEncodeNumList'#input('Enter File Name (do not add extension): ')
 filename=filename+'.txt'
 eraseFile='N'#input('Erase original file? (Y or N): ')
 userFile=open(filename,'r')
@@ -75,7 +75,7 @@ else:
 		for item in userFileContent:
 			if item!=None:
 				decodedChar=charNumDecode(item)
-
+				output+=str(intToChar(decodedChar))
 #Cleaning up
 if eraseFile=='Y' or eraseFile=='y':
 	userFile=open(filename,'w')
@@ -87,6 +87,8 @@ else:
 	status='Decrypted'
 outFileName=status+filename
 outFile=open(outFileName,'w')
+outFile.write(output)
+outFile.close()
 print(filename,status,' with key: ',keyObj.key)
 
 #Clears Variables and closes files
